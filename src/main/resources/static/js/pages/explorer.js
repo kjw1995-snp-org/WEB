5cc2bdeaf78aed0f2f20ce4269d4be5f2b543d9e
kjw1995 <114547095+kjw1995@users.noreply.github.com>
2024-03-04 00:13:05 +0900

    Initializing static resources

---
+++ src/main/resources/static/js/pages/explorer.js
@@ -0,0 +1,16 @@
+/*! explorer.js | Friendkit | © Css Ninja. 2019-2020 */
+
+/* ==========================================================================
+Explorer menu js file
+========================================================================== */
+
+$(function () {
+  'use strict'
+
+  if ($('.explorer-menu').length) {
+    //Open explorer menu
+    $('#explorer-trigger, #mobile-explorer-trigger').on('click', function () {
+      $('.explorer-menu').toggleClass('is-active')
+    })
+  }
+})
