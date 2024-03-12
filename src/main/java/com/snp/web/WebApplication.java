2441c0a26f44ad6c25409dafbc9771b9a7c0ca24
kimjungwoo <wjdddn312@hellofunding.co.kr>
2024-03-12 17:51:27 +0900

    Eureka 도입

--- src/main/java/com/snp/web/WebApplication.java
+++ src/main/java/com/snp/web/WebApplication.java
@@ -4,11 +4,13 @@ import com.snp.web.configuration.properties.ConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableConfigurationProperties(ConfigProperties.class)
+@EnableDiscoveryClient
 public class WebApplication {
 
     public static void main(String[] args) {
